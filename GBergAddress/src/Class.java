50859f3450cf5128b07148b749f6e3464c0309f4
andrewstagnaro <42977270+andrewstagnaro@users.noreply.github.com>
2018-11-05 13:35:35 -0500

    Add files via upload

---
+++ GBergAddress/src/Class.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Drew Stagnaro
+ * identify longest word and the average word in the gettysburg address
+ *
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Class
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+	Scanner file = new Scanner(new File("GettysburgAddress.txt"));
+
+	
+	
+	
+	
+	}
+
+}
